142ebc97c7ecf85b2edb19553683845b7e132b68
Victoria Cristina Silva <victoriacristina@protonmail.com>
2022-04-01 13:38:08 -0300

    JS done on login page

--- public/js/index.js
+++ public/js/index.js
@@ -1,153 +1,39 @@
-//const myName = "Victoria Cristina";
-let myName2 = "Morphea Yuno";
-let personDefault = {
-  name: "Victoria Cristina",
-  age: "26",
-  profession: "English Teacher",
-};
+const myModal = new bootstrap.Modal("#registerModal");
 
-let nomes = ["Victoria Cristina", "Luna Lima", "Galatea"];
+//CREATE ACCOUNT
 
-let pessoasListaVazia = [];
+document
+  .getElementById("create-acc-form")
+  .addEventListener("submit", function (e) {
+    e.preventDefault();
 
-let pessoas = [
-  {
-    name: "Victoria Cristina",
-    age: "26",
-    profession: "English Teacher",
-  },
-  {
-    name: "Victoria Cristina",
-    age: "26",
-    profession: "English Teacher",
-  },
-];
+    const email = document.getElementById("create-email-input").value;
+    const password = document.getElementById("create-password-input").value;
 
-function adicionarPessoa(pessoa) {
-  pessoas.push(pessoa);
-}
+    console.log(email, password);
 
-function imprimirPessoas() {
-  console.log("--------IMPRIMIR PESSOAS----------");
-  pessoas.forEach((item) => {
-    console.log("Nome:");
-    console.log(item.name);
+    if (email.length < 5) {
+      alert("Ooops! Is that really an e-mail?");
+      return;
+    }
 
-    console.log("Age:");
-    console.log(item.age);
-
-    console.log("Profession:");
-    console.log(item.profession);
-    console.log(item);
+    if (password.length < 4) {
+      alert("Your password must have at least 4 characters");
+      return;
+    } else {
+      saveAccount({
+        login: email,
+        password: password,
+        transactions: [],
+      });
+      myModal.hide();
+      alert("Account created succesfully");
+    }
   });
+
+function saveAccount(data) {
+  localStorage.setItem(data.login, JSON.stringify(data));
 }
-
-imprimirPessoas();
-
-console.log(pessoas);
-
-adicionarPessoa({
-  name: "Victoria Cristina",
-  age: "260",
-  profession: "English Teacher",
-});
-
-console.log(pessoas);
-
-// console.log("Nome:");
-// console.log(person.name);
-
-// console.log("Idade:");
-// console.log(person.age);
-
-// console.log("Profissão:");
-// console.log(person.profession);
-
-// function printPerson(person) {
-//   console.log("Nome:");
-//   console.log(person.name);
-
-//   console.log("Idade:");
-//   console.log(person.age);
-
-//   console.log("Profissão:");
-//   console.log(person.profession);
-// }
-
-// printPerson(personDefault);
-
-// imprimirPessoa({
-//   nome: "Luna Lima",
-//   idade: "24",
-//   trabalho: "Art Director",
-// });
-
-// // let people = ["Victoria Cristina", "Luna Lima", "Galatea Mortífera"];
-
-// let listaVazia = [];
-
-// let people = [
-//   {
-//     name: "Victoria Cristina",
-//     age: "26",
-//     profession: "English Teacher",
-//   },
-
-//   {
-//     name: "Luna Lima",
-//     age: "24",
-//     profession: "Art Director",
-//   },
-// ];
-
-// function addPerson() {
-//   people.push(people);
-// }
-
-// addPerson({
-//   name: "Landrugo",
-//   age: "100",
-//   profession: "Gravekeeper",
-// });
-
-// console.log(people);
-
-// console.log("Initial name:");
-// console.log(myName2);
-
-// myName2 = "Victoria Cristina";
-
-// console.log("Altered name:");
-// console.log(myName2);
-
-// function changeName() {
-//   myName2 = "Luanna Brune";
-//   console.log("Altered name:");
-//   console.log(myName2);
-// }
-
-// function getAndChangeName(newName) {
-//   myName2 = newName;
-//   console.log("Altered value receiving name");
-//   console.log(myName2);
-// }
-
-// // //getAndChangeName("Maria");
-// // getAndChangeName("Luana");
-// // getAndChangeName("Anastácia");
-
-// function printPerson(person) {
-//   console.log("Name:");
-//   console.log(person.name);
-
-//   console.log("Age:");
-//   console.log(person.age);
-
-//   console.log("Profession:");
-//   console.log(person.profession);
-// }
-
-// function printPerson(person) {}
 
 // Bootstrap Tooltip
 var tooltipTriggerList = [].slice.call(
